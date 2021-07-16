9714ee47d2827149bcfce3a473a3e537656a5922
xsharonhe <xsharonhe@gmail.com>
2021-07-16 16:40:00 -0400

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,41 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Sharon He</title>
         <meta name="title" content="Sharon He" />
+        <link 
+          rel="apple-touch-icon" 
+          sizes="180x180" 
+          href="/apple-touch-icon.png"
+        />
+        <link 
+          rel="icon" 
+          type="image/png" 
+          sizes="32x32" 
+          href="/favicon-32x32.png"
+        />
+        <link 
+          rel="icon" 
+          type="image/png" 
+          sizes="16x16" 
+          href="/favicon-16x16.png"
+        />
+        <link 
+          rel="manifest" 
+          href="/site.webmanifest"
+        />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" type="image/x-icon" />
+        <link 
+          rel="mask-icon" 
+          href="/safari-pinned-tab.svg" 
+          color="#5bbad5"
+        />
+        <meta 
+          name="msapplication-TileColor" 
+          content="#2b5797"
+        />
+        <meta 
+          name="theme-color" 
+          content="#f7faff"
+        />
       </Head>
       <ThemeProvider theme={baseTheme}>
         <Component {...pageProps} />

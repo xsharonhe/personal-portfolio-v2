43627ef017da0a170a9d966a29d61416780de0cc
xsharonhe <xsharonhe@gmail.com>
2021-07-23 16:08:28 -0400

    Global styles changed

--- theme/GlobalStyles.ts
+++ theme/GlobalStyles.ts
@@ -4,9 +4,9 @@ export const GlobalStyle = createGlobalStyle<{theme: DefaultTheme}>`
     @font-face {
         font-family: 'Calibre';
         src: local('Calibre Regular'), local('Calibre-Regular'),
-            url('Calibre-Regular.woff2') format('woff2'),
-            url('Calibre-Regular.woff') format('woff'),
-            url('Calibre-Regular.ttf') format('truetype');
+            url('../public/fonts/Calibre-Regular.woff2') format('woff2'),
+            url('../public/fonts/Calibre-Regular.woff') format('woff'),
+            url('../public/fonts/Calibre-Regular.ttf') format('truetype');
         font-weight: 400;
         font-style: normal;
     }
